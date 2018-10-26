78ecb343f4d0e2d339db00038167aa49c6fc0dd7
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-10-26 12:22:20 +0100

    index page is rendering on page

--- src/components/IndexRoute.js
+++ src/components/IndexRoute.js
@@ -6,7 +6,9 @@ import axios from 'axios';
 class IndexRoute extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      data: []
+    };
   }
 
   componentDidMount(){
@@ -21,8 +23,27 @@ class IndexRoute extends React.Component {
   render() {
     console.log(this.state);
     return (
-      <div>
-        
+      <div className="columns is-multiline">
+        {this.state.data.map(share =>
+          <div className="card column is-one-quarter-desktop is-one-third-tablet" key={share.id}>
+            <Link to={'share.image'}>
+              <div className="card-image">
+                <figure className="image is-4by3">
+                  <img src={share.image} alt="Shared item image" />
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="media">
+                  <div className="content-title">
+                    <h2 className="title is-6 share-title">{share.title}</h2>
+                    <h2 className="title is-6 share-username">{share.username}</h2>
+                    <p className="subtitle is-6">{share.likes}</p>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </div>
+        )}
       </div>
     );
   }
